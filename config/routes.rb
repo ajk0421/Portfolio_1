9bf4cbff683af859d4f3c513dead36e0709b25ab
ajk0421 <ajk.spirituallounge@gmai.com>
2019-03-22 21:05:07 +0900

    add_serach_method

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: :show
 
   resources :artists do
+    member do
+      get :gozyu_on
+    end
     post :confirm, action: :confirm_new, on: :new
   end
 end
