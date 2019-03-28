7cb658d214cac55e74e2f6a426de7c484ca48e96
ajk0421 <ajk.spirituallounge@gmai.com>
2019-03-28 12:29:32 +0900

    first-commit

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,17 @@ Bundler.require(*Rails.groups)
 
 module Portfolio1
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.generators do |g|
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework false
+      g.test_framework :rspec
+    end
+
+
   end
 end
