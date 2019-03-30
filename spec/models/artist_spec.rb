93f13761a6922ee8bffadcc28ffad860e1a33111
ajk0421 <ajk.spirituallounge@gmai.com>
2019-03-31 02:00:31 +0900

    first_commit

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+
+  context "name を指定してるとき" do
+    it "アーティストが作成される" do
+      # sign_in←上手くいかないのでコメントアウト
+      artist = Artist.new(name: "テスト", furi_gana: "テスト", image: Rails.root.join("public/test.jpg").open, place: "札幌", profile: "テスト", user_id: 1)
+      expect(artist.valid?).to eq true
+
+    end
+  end
 end
