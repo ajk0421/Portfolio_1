aef4ad145328cdf7bb37b64fe54c9a2ecf04d00c
ajk0421 <ajk.spirituallounge@gmai.com>
2019-04-02 22:17:14 +0900

    add_artist_valid_test

--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :artist do
-    
+    name{Faker::Music::RockBand.name}
+    furi_gana{"カタカナ"}
+    image{Rails.root.join("spec/fixtures/test.jpg").open}
+    place{Faker::Address.city}
+    profile{Faker::String.random}
+
+  end
+
+  factory :user do
+    name{Faker::Name.name}
+    email{Faker::Internet.email}
+    password{Faker::Alphanumeric.alpha 10}
   end
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,14 +2,67 @@ require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
+  let(:user){create(:user)}
+  let(:artist){build(:artist,user_id: user.id)}
 
-
-  context "name を指定してるとき" do
+  context "必須項目を指定してるとき" do
     it "アーティストが作成される" do
-      # sign_in←上手くいかないのでコメントアウト
-      artist = Artist.new(name: "テスト", furi_gana: "テスト", image: Rails.root.join("public/test.jpg").open, place: "札幌", profile: "テスト", user_id: 1)
-      expect(artist.valid?).to eq true
-
+      expect(artist).to be_valid
     end
   end
+
+  context "nameが指定されていない" do
+    it "エラーになる" do
+      artist.name = nil
+      artist.valid?
+
+      expect(artist.errors.messages[:name]).to include("を入力して下さい。")
+    end
+  end
+
+  context "furi_ganaが指定されていない" do
+    it "エラーになる" do
+      artist.furi_gana = nil
+      artist.valid?
+
+      expect(artist.errors.messages[:furi_gana]).to include("を入力して下さい。", "は全角カタカナで入力して下さい")
+    end
+  end
+
+  context "furi_ganaが全角カタカナ以外で指定されている" do
+    it "エラーになる" do
+      artist.furi_gana = [Faker::Name.name,Faker::Alphanumeric.alpha,"ｶﾀｶﾅ"].sample
+      artist.valid?
+
+      expect(artist.errors.messages[:furi_gana]).to include("は全角カタカナで入力して下さい")
+    end
+  end
+
+  context "imageが指定されていない" do
+    it "エラーになる" do
+      artist.image = nil
+      artist.valid?
+
+      expect(artist.errors.messages[:image]).to include("を入力して下さい")
+    end
+  end
+
+  context "placeが指定されていない" do
+    it "エラーになる" do
+      artist.place = nil
+      artist.valid?
+
+      expect(artist.errors.messages[:place]).to include("を入力して下さい。")
+    end
+  end
+
+  context "profileが指定されていない" do
+    it "エラーになる" do
+      artist.profile = nil
+      artist.valid?
+
+      expect(artist.errors.messages[:profile]).to include("を入力して下さい。")
+    end
+  end
+
 end
